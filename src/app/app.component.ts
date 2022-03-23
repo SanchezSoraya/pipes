06f7be3cbc697f53d1ff78d1c39338ae5a556c65
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-03-23 09:20:31 -0500

    tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string= 'Capital America'; 
+  nombre2: string= 'aLeJandRa CasTrO'
+
+
+  arreglo =[1,2,3,4,5,6,7,8,9,10];
+  PI: number=Math.PI;
+  porcentaje: number= 0.234; 
+  salario: number=1234.5; 
+  fecha: Date = new Date();
+  activar: boolean= true;
+
+  idioma : string= 'en'; 
+  videoUrl: string='https://www.youtube.com/embed/tP8JiVUiyDo'; 
+
+  heroe = {
+    nombre: 'mujer maravilla',
+    clave: 'lazo',
+    edad: 300,
+    direccion: {
+      calle: 'segunda', 
+      casa: 30 
+    }
+  }
+
+ valorPromesa = new Promise<string>( (resolve) => {
+
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+
+
 }
---
+++ src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contrasena'
+})
+export class ContrasenaPipe implements PipeTransform {
+
+  transform(value: string, mostrar: boolean=false): string {
+  
+
+   return (mostrar) ? '*'.repeat(value.length): value ;
+  }
+
+}
